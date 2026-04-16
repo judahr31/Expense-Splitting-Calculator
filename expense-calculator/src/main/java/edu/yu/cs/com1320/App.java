bc177e5180d22dd1125afad60e884a247fdee9b3
judahr31 <judahrosenthal31@gmail.com>
2026-04-16 16:37:24 -0400

    set up env with maven

---
+++ expense-calculator/src/main/java/edu/yu/cs/com1320/App.java
@@ -0,0 +1,13 @@
+package edu.yu.cs.com1320;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
