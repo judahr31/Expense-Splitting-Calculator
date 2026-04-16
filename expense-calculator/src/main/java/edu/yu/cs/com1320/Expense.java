1e2d52891bf3cf0578a5225321cb55783ab5ce7e
judahr31 <judahrosenthal31@gmail.com>
2026-04-16 16:49:00 -0400

    added constructor to Expense.java

--- expense-calculator/src/main/java/edu/yu/cs/com1320/Expense.java
+++ expense-calculator/src/main/java/edu/yu/cs/com1320/Expense.java
@@ -1,5 +1,17 @@
 package edu.yu.cs.com1320;
 
 public class Expense {
+    private String expenseId;
+    private String description;
+    private double totalAmount;
+    private User payer;
+    private List<User> participants;
     
+    public Expense(String des, double amt, User payer) {
+        
+    }
+    // Getters and Setters...
+    public List<User> getParticipants() {
+        return new ArrayList<>(this.participants);
+    }
 }
