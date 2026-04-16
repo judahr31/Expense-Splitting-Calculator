b41b8b8902257fdbba6be7d5038b9bf7ff28eef8
judahr31 <judahrosenthal31@gmail.com>
2026-04-16 16:58:58 -0400

    updated pom for junit

--- expense-calculator/src/test/java/edu/yu/cs/com1320/UserTest.java
+++ expense-calculator/src/test/java/edu/yu/cs/com1320/UserTest.java
@@ -1,5 +1,9 @@
 package edu.yu.cs.com1320;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UserTest {
     
 }
