cd86997967c3016d7b96187986d6e6f9236d4894
judahr31 <judahrosenthal31@gmail.com>
2026-04-21 16:08:15 -0400

    updated debt record and added skeleton methods to group

--- expense-calculator/src/main/java/edu/yu/cs/com1320/Debt.java
+++ expense-calculator/src/main/java/edu/yu/cs/com1320/Debt.java
@@ -1,4 +1,3 @@
 package edu.yu.cs.com1320;
 
-
 public record Debt(User debtor, User creditor, double amount) {}
--- expense-calculator/src/main/java/edu/yu/cs/com1320/Group.java
+++ expense-calculator/src/main/java/edu/yu/cs/com1320/Group.java
@@ -36,6 +36,34 @@ public class Group {
          */
     }
 
+    /**
+     * Logs an expense and immediately updates the net balances of everyone involved.
+     */
+    public void addExpense(Expense expense) {
+        // TODO: 1. Add the expense to the expenseHistory list
+        // TODO: 2. Calculate the split amount (total / number of participants)
+        // TODO: 3. Credit the payer's net balance (add the total amount)
+        // TODO: 4. Debit everyone in the split (subtract the split amount from their balance)
+    }
+
+    /**
+     * The core algorithm: Matches debtors to creditors using Priority Queues 
+     * to minimize the total number of transactions.
+     * * @return A list of instructions (Debts) to settle all accounts.
+     */
+    public List<Debt> calculateSettlement() {
+        List<Debt> settlements = new ArrayList<>();
+
+        // TODO: 1. Create a Max-Heap for creditors (users with positive balance)
+        // TODO: 2. Create a Max-Heap for debtors (users with negative balance)
+        // TODO: 3. Populate both heaps by iterating through this.members.values()
+        // TODO: 4. Create a while loop that runs until one or both heaps are empty
+        // TODO: 5. Inside the loop, pop the top of both heaps, find the min amount, 
+        //          create a Debt object, update balances, and push them back if they still have a balance.
+
+        return settlements; // Returns the generated list of instructions
+    }
+
     protected void setName(String name){
         this.name = name;
     }
